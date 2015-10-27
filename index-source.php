fca25ae9a9ec361a42743fe8a27e58ea4a3fd33b
Fiona84 <fiona_17@interia.pl>
2015-10-27 16:28:51 +0100

    first commit

---
+++ index-source.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Czytaj książki Online</title>
+</head>
+<body>
+    <header>
+    </header>
+    <section id="introduction">
+        <div>
+            <h1>Czytaj Książki online</h1>
+            <p>
+                Skorzystaj z naszych usług, wypożyczaj i czytaj książki online.
+                Wybierz bezapelacyjnie najlepiej zoptymalizowane rozwiązanie
+                dostępne na rynku.
+            </p>
+        </div>
+    </section>
+    <section id="press">
+        <div>
+            <h1 class="">Mówią o nas</h1>
+            <div>
+                <div>
+                  <h2>Onet.pl</h2>
+                  <img src="http://www.kupujnasze.pl/wp-content/uploads/2010/03/logo_onetpl_kropka_odstep.jpg"/>
+                  <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
+                  <p><a href="#" role="button">Czytaj więcej »</a></p>
+                </div>
+            <div>
+              <h2>wp.pl</h2>
+              <img src="http://gomobi.pl/data/uploads/2012/03/wirtualna_polska.jpg"/>
+              <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
+              <p><a href="#" role="button">Czytaj więcej »</a></p>
+           </div>
+            <div>
+              <h2>Kurier UEK</h2>
+              <img src="http://cfm.uek.krakow.pl/media/images/8d/55/c032f96f30ebb23a22f96b221f90.jpeg"/>
+              <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
+              <p><a href="#" role="button">Czytaj więcej »</a></p>
+            </div>
+          </div>
+        </div>
+       
+    </section>
+    <section id="stats">
+        <div>
+            <h1>Statystyki</h1>
+            <div>
+                <div>
+                    <div>
+                        <i></i>
+                        <small>16k użytkowników dziennie</small>
+                    </div>
+                </div>
+                <div>
+                   <div>
+                        <i></i>
+                        <small>320k odsłon</small>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <i></i>
+                        <small>100k polubień</small>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <i></i>
+                        <small>1M pobrań</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="pricing">
+        <div>
+        <h1>Ceny</h1>
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <h3>
+                            Brown</h3>
+                    </div>
+                    <div>
+                        <div>
+                            <h1>
+                                10zł<span>/miesiąc</span></h1>
+                            <small>1 miesiąc za darmo</small>
+                        </div>
+                        <table>
+                            <tr>
+                                <td>
+                                    1 konto
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    1 ksiązka
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    100MB w schowku
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Natychmiastowe wypożyczenia
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Raporty miesięczne
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div>
+                        <a href="#" role="button">Wykup Subskrypcję</a>
+                        1 miesiąc za darmo</div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <h3>
+                            Silver</h3>
+                    </div>
+                    <div>
+                        <div>
+                            <h1>
+                                40zł<span>/miesiąc</span></h1>
+                            <small>1 miesiąc za darmo</small>
+                        </div>
+                        <table>
+                            <tr>
+                                <td>
+                                    5 konto
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    100 ksiązka
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    1GB w schowku
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Natychmiastowe wypożyczenia
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Raporty miesięczne
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div>
+                        <a href="#" role="button">Wykup Subskrypcję</a>
+                        1 miesiąc za darmo</div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <h3>
+                            Gold</h3>
+                    </div>
+                    <div>
+                        <div>
+                            <h1>
+                                100zł<span>/miesiąc</span></h1>
+                            <small>1 miesiąc za darmo</small>
+                        </div>
+                        <table>
+                            <tr>
+                                <td>
+                                    100 konto
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    nieograniczona liczba książek
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    100MB w schowku
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Natychmiastowe wypożyczenia
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Raporty miesięczne
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div>
+                        <a href="#" role="button">Wykup Subskrypcję</a>
+                        1 miesiąc za darmo</div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="recent">
+        <h1>Najnowsze pozycje</h1>
+        <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Autory</th>
+                  <th>Tytuł</th>
+                  <th>Język</th>
+                  <th>Liczba stron</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Jak zostać ninja?</td>
+                  <td>Japoński</td>
+                  <td>1500</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Mr. iksiński</td>
+                  <td>Jak zostać git guru?</td>
+                  <td>Polski</td>
+                  <td>20</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Mr. Nowak</td>
+                  <td>Gdzie jest kowalski?</td>
+                  <td>Polski</td>
+                  <td>200</td>
+                </tr>
+              </tbody>
+            </table>
+        </div>
+    </section>
+    <section id="order">
+        <h1>Skontaktuj się</h1>
+        <div>
+            <form role="form" action="" method="post" >
+                <div>
+                  <div>
+                    <label>Imię</label>
+
+                      <input type="text" name="InputName" id="InputName" placeholder="Wpisz imię" required>
+    
+                  <div>
+                    <label for="InputEmail">Email</label>
+
+                       <input type="email" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+    
+                  <div>
+                    <label for="InputMessage">Wiadomość</label>
+                      <textarea name="InputMessage" id="InputMessage" rows="5" required></textarea>
+    
+                  <input type="submit" name="submit" id="submit" value="Wyślij" >
+                </div>
+              </form>
+            </div>    
+    </section>
+    <footer>
+    </footer>
+</body>
+</html>
