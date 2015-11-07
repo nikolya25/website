1bceb809cfae060612e821553e6dd6462b1d6760
Mykola Shchur <kolya_shchur@mail.ru>
2015-11-07 15:13:28 +0100

    gotowe

--- index-source.php
+++ index-source.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-    	<meta charset="UTF-8">
-	<title>Czytaj książki Online</title>
-</head>
+<meta charset="UTF-8"/>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
 
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="./styles.css"/>
+    <title>Czytaj książki Online</title>
+</head>
 <body>
     <header>
     </header>
     <section id="introduction">
+	
         <div class="container text-center">
             <h1>Czytaj Książki online</h1>
-            <p>
+            <p class="lead">
                 Skorzystaj z naszych usług, wypożyczaj i czytaj książki online.
                 Wybierz bezapelacyjnie najlepiej zoptymalizowane rozwiązanie
                 dostępne na rynku.
@@ -24,56 +28,56 @@
         </div>
     </section>
     <section id="press">
-        <div>
+        <div class="container">
             <h1 class="">Mówią o nas</h1>
-            <div class="container text-center">
-                <div>
+            <div class="row">
+                <div class="col-lg-4">
                   <h2>Onet.pl</h2>
-                  <img src="http://www.kupujnasze.pl/wp-content/uploads/2010/03/logo_onetpl_kropka_odstep.jpg"/>
+                  <img class="img-responsive" src="http://www.kupujnasze.pl/wp-content/uploads/2010/03/logo_onetpl_kropka_odstep.jpg"/>
                   <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
-                  <p><a href="#" role="button">Czytaj więcej »</a></p>
+                  <p><a class="btn-default"  href="#" role="button">Czytaj więcej »</a></p>
                 </div>
-            <div>
+            <div class="col-lg-4">
               <h2>wp.pl</h2>
-              <img src="http://gomobi.pl/data/uploads/2012/03/wirtualna_polska.jpg"/>
+              <img class="img-responsive"  src="http://gomobi.pl/data/uploads/2012/03/wirtualna_polska.jpg"/>
               <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
-              <p><a href="#" role="button">Czytaj więcej »</a></p>
+              <p><a class="btn-default"  href="#" role="button">Czytaj więcej »</a></p>
            </div>
-            <div>
+            <div class="col-lg-4">
               <h2>Kurier UEK</h2>
-              <img src="http://cfm.uek.krakow.pl/media/images/8d/55/c032f96f30ebb23a22f96b221f90.jpeg"/>
+              <img class="img-responsive" src="http://cfm.uek.krakow.pl/media/images/8d/55/c032f96f30ebb23a22f96b221f90.jpeg"/>
               <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
-              <p><a href="#" role="button">Czytaj więcej »</a></p>
+              <p><a class="btn-default" href="#" role="button">Czytaj więcej »</a></p>
             </div>
           </div>
         </div>
        
     </section>
     <section id="stats">
-        <div>
+        <div class="container text-center">
             <h1>Statystyki</h1>
-            <div>
-                <div>
-                    <div>
-                        <i></i>
+            <div class="row badges-container">
+                <div class="col-md-3 col-xs-6">
+                    <div class="center-block stat-badge red">
+                        <i class="glyphicon glyphicon-user"></i>
                         <small>16k użytkowników dziennie</small>
                     </div>
                 </div>
-                <div>
-                   <div>
-                        <i></i>
+                <div class="col-md-3 col-xs-6">
+                   <div class="stat-badge center-block green">
+                        <i class="glyphicon glyphicon-signal"></i>
                         <small>320k odsłon</small>
                     </div>
                 </div>
-                <div>
-                    <div>
-                        <i></i>
+                <div class="col-md-3 col-xs-6">
+                    <div class="stat-badge center-block blue">
+                        <i class="glyphicon glyphicon-thumbs-up"></i>
                         <small>100k polubień</small>
                     </div>
                 </div>
-                <div>
-                    <div>
-                        <i></i>
+                <div class="col-md-3 col-xs-6">
+                    <div class="stat-badge center-block yellow">
+                        <i class="glyphicon glyphicon-phone"></i>
                         <small>1M pobrań</small>
                     </div>
                 </div>
@@ -81,33 +85,33 @@
         </div>
     </section>
     <section id="pricing">
-        <div>
+        <div class="container text-center">
         <h1>Ceny</h1>
-        <div>
-            <div>
-                <div>
-                    <div>
-                        <h3>
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">
                             Brown</h3>
                     </div>
-                    <div>
+                    <div class="panel-body">
                         <div>
                             <h1>
                                 10zł<span>/miesiąc</span></h1>
                             <small>1 miesiąc za darmo</small>
                         </div>
-                        <table>
+                        <table class="table">
                             <tr>
                                 <td>
                                     1 konto
                                 </td>
                             </tr>
-                            <tr>
+                            <tr class="active">
                                 <td>
                                     1 ksiązka
                                 </td>
                             </tr>
-                            <tr>
+                            <tr class="active">
                                 <td>
                                     100MB w schowku
                                 </td>
@@ -117,42 +121,42 @@
                                     Natychmiastowe wypożyczenia
                                 </td>
                             </tr>
-                            <tr>
+                            <tr class="active">
                                 <td>
                                     Raporty miesięczne
                                 </td>
                             </tr>
                         </table>
                     </div>
-                    <div>
-                        <a href="#" role="button">Wykup Subskrypcję</a>
+                    <div class="panel-footer">
+                        <a href="#" class="btn btn-success" role="button">Wykup Subskrypcję</a>
                         1 miesiąc za darmo</div>
                 </div>
             </div>
-            <div>
-                <div>
-                    <div>
-                        <h3>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="panel panel-success">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">
                             Silver</h3>
                     </div>
-                    <div>
+                    <div class="panel-body">
                         <div>
                             <h1>
                                 40zł<span>/miesiąc</span></h1>
                             <small>1 miesiąc za darmo</small>
                         </div>
-                        <table>
+                        <table class="table">
                             <tr>
                                 <td>
-                                    5 konto
+                                    5 kont
                                 </td>
                             </tr>
-                            <tr>
+                            <tr class="active">
                                 <td>
-                                    100 ksiązka
+                                    100 ksiązek
                                 </td>
                             </tr>
-                            <tr>
+                            <tr class="active">
                                 <td>
                                     1GB w schowku
                                 </td>
@@ -162,42 +166,42 @@
                                     Natychmiastowe wypożyczenia
                                 </td>
                             </tr>
-                            <tr>
+                            <tr class="active">
                                 <td>
                                     Raporty miesięczne
                                 </td>
                             </tr>
                         </table>
                     </div>
-                    <div>
-                        <a href="#" role="button">Wykup Subskrypcję</a>
+                    <div class="panel-footer">
+                        <a href="#"class="btn btn-success" role="button">Wykup Subskrypcję</a>
                         1 miesiąc za darmo</div>
                 </div>
             </div>
-            <div>
-                <div>
-                    <div>
-                        <h3>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="panel panel-warning">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">
                             Gold</h3>
                     </div>
-                    <div>
+                    <div class="panel-body">
                         <div>
                             <h1>
                                 100zł<span>/miesiąc</span></h1>
                             <small>1 miesiąc za darmo</small>
                         </div>
-                        <table>
+                        <table class="table">
                             <tr>
                                 <td>
-                                    100 konto
+                                    100 kont
                                 </td>
                             </tr>
-                            <tr>
+                            <tr class="active">
                                 <td>
                                     nieograniczona liczba książek
                                 </td>
                             </tr>
-                            <tr>
+                            <tr class="active">
                                 <td>
                                     100MB w schowku
                                 </td>
@@ -207,15 +211,15 @@
                                     Natychmiastowe wypożyczenia
                                 </td>
                             </tr>
-                            <tr>
+                            <tr class="active">
                                 <td>
                                     Raporty miesięczne
                                 </td>
                             </tr>
                         </table>
                     </div>
-                    <div>
-                        <a href="#" role="button">Wykup Subskrypcję</a>
+                    <div class="panel-footer">
+                        <a href="#" class="btn btn-success" role="button">Wykup Subskrypcję</a>
                         1 miesiąc za darmo</div>
                 </div>
                 </div>
@@ -223,9 +227,9 @@
         </div>
     </section>
     <section id="recent">
-        <h1>Najnowsze pozycje</h1>
-        <div>
-            <table>
+        <h1 class="text-center">Najnowsze pozycje</h1>
+        <div class="container">
+            <table class="table table-hover">
               <thead>
                 <tr>
                   <th>#</th>
@@ -262,26 +266,25 @@
         </div>
     </section>
     <section id="order">
-        <h1>Skontaktuj się</h1>
-        <div>
-            <form role="form" action="" method="post" >
-                <div>
-                  <div>
+        <h1 class="text-center">Skontaktuj się</h1>
+        <div class="container">
+            <form role="form" action="" method="post" class="form-horizontal">
+                <div class="col-lg-6">
+                  <div class="form-group">
                     <label>Imię</label>
-
-                      <input type="text" name="InputName" id="InputName" placeholder="Wpisz imię" required>
-    
-                  <div>
+                      <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Wpisz imię" required>
+                  </div>
+                  <div class="form-group">
                     <label for="InputEmail">Email</label>
-
-                       <input type="email" id="exampleInputEmail2" placeholder="jane.doe@example.com">
-    
-                  <div>
+                       <input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+                  </div>
+                  <div class="form-group">
                     <label for="InputMessage">Wiadomość</label>
-                      <textarea name="InputMessage" id="InputMessage" rows="5" required></textarea>
-    
-                  <input type="submit" name="submit" id="submit" value="Wyślij" >
+                      <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" required></textarea>
+                  </div>
+                  <input type="submit" name="submit" id="submit" class="btn btn-info pull-right" value="Wyślij" >
                 </div>
+               </div>
               </form>
             </div>    
     </section>
